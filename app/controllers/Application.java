feac5c3dfeed8c042921f1f41d57964272c7c4d5
sergei <melnick.s.a@gmail.com>
2012-02-29 17:24:18 +0300

    added file reader

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -3,6 +3,10 @@ package controllers;
 import play.*;
 import play.mvc.*;
 
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.util.*;
 
 import models.*;
@@ -12,5 +16,26 @@ public class Application extends Controller {
     public static void index() {
         render();
     }
+    
+    public static void upload(String label,File uploadFile){
+        //uploadFile = new File();
+        String result = reader(uploadFile);
 
+        render(label,result);
+    }
+    
+    private static String reader(File file){
+        int ch;
+        StringBuffer strContent = new StringBuffer("");
+        FileInputStream fin = null;
+        try {
+            fin = new FileInputStream(file);
+            while ((ch = fin.read()) != -1)
+                strContent.append((char) ch);
+            fin.close();
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+        return  strContent.toString();
+    }
 }
